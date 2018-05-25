a3cafb69fc61704d1dd66dce309b9153617ea75e
Donnadieu <donnadieu.86@gmail.com>
2018-05-25 14:41:28 -0400

    working on authentication

--- rails-etherum-api/app/controllers/v1/registrations_controller.rb
+++ rails-etherum-api/app/controllers/v1/registrations_controller.rb
@@ -1,9 +1,6 @@
 class V1::RegistrationsController < Devise::RegistrationsController
 
   private
-    # def user_params
-    #  params.require(:user).permit(:email, :password)
-    # end
 
     def resource_name
      :user
