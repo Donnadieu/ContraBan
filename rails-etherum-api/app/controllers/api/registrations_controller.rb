a129afdbc69ad9ad3188b1d30ddb52ada8f9331d
Donnadieu <donnadieu.86@gmail.com>
2018-05-27 14:17:07 -0400

    validating password precense

--- rails-etherum-api/app/controllers/api/registrations_controller.rb
+++ rails-etherum-api/app/controllers/api/registrations_controller.rb
@@ -5,26 +5,30 @@ class Api::RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user.as_json(only: [:id, :email, :authentication_token]), status: :created
-      return
-    else
-      warden.custom_failure!
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @user.id === params[:id].to_i
-      if @user.update(user_params)
-        render json: @user.as_json(only: [:id, :email, :authentication_token])
+    if !params[:user][:password].empty?
+      if @user.save
+        render json: @user.as_json(only: [:id, :email, :authentication_token]), status: :created
         return
       else
         warden.custom_failure!
         render json: @user.errors, status: :unprocessable_entity
       end
     else
-      render json:{ message: "Unable to perform this action"}, status: 401
+      render json:{ errors: "Password can't be blank"}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.id === params[:id].to_i
+      if @user.update(user_params) && !params[:user][:password].empty?
+        render json: @user.as_json(only: [:id, :email, :authentication_token])
+        return
+      else
+        warden.custom_failure!
+        render json:{ errors: "Wrong parameters"}, status: :unprocessable_entity
+      end
+    else
+      render json:{ errors: "Unable to perform this action"}, status: 401
     end
   end
 
--- rails-etherum-api/app/models/user.rb
+++ rails-etherum-api/app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable
    validates :email, uniqueness: true
    validates :email, presence: true
-   validates :password, presence: true
 end
