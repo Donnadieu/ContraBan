69eb6055fa0df72e4e627e3f3bb5de2753bac8dc
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 15:36:02 -0400

    Change params structure to handle request

--- rails-etherum-api/app/controllers/api/sessions_controller.rb
+++ rails-etherum-api/app/controllers/api/sessions_controller.rb
@@ -2,9 +2,9 @@ class Api::SessionsController < ApplicationController
   acts_as_token_authentication_handler_for User, except: [:create]
 
   def create
-    @user = User.find_by(email: params[:user][:email])
+    @user = User.find_by(email: params[:email])
 
-    if @user&.valid_password?(params[:user][:password])
+    if @user&.valid_password?(params[:password])
       renew_authentication_token!
       render json: @user.as_json(only: [:id, :email, :authentication_token]), status: 200
     else
