68990248a1b363ddfa2ddd4721c9b29afa104e6a
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 15:35:08 -0400

    Create action for login

--- rails-etherum-client/src/actions/index.js
+++ rails-etherum-client/src/actions/index.js
@@ -1,7 +1,11 @@
 export function login(values) {
   fetch('http://localhost:3000/api/auth/login', {
     method: "POST",
-    body: {"user": {"email": values.email, "password": values.password} }
+    body: {
+      "user": {
+      "email": values.email,
+      "password": values.password}
+    }
   })
   .then(response => {
     console.log(response);
