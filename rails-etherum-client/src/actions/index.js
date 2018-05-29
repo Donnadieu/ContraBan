a0d63ed51aa9aa3341db1d9054db9893c8962a32
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 18:12:02 -0400

    Initialize React app

--- rails-etherum-client/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export function login(values) {
-  fetch(`http://localhost:3000/api/auth/login?email=${values.email}&password=${values.password}`, {
-    method: "POST"
-  })
-  .then(response => {
-    console.log(response.json());
-  })
-}
--- rails-etherum-client/src/index.js
+++ rails-etherum-client/src/index.js
@@ -1,26 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import './index.css'
-import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
-import store from './store.js'
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>,
-  document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// var store = {
-//   currentUser: {
-//     id: 'id',
-//     email: 'email',
-//     auth_token: 'token',
-//     has_login_credentials: false
-//     contracts: []
-//   }
-// }
