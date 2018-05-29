af90fbc734607ee9a3d45295ae42ddc0c6cc14e7
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 12:00:01 -0400

    setting up facebook omniauth

---
+++ rails-etherum-api/app/controllers/api/omniauth_callbacks_controller.rb
@@ -0,0 +1,21 @@
+class Api::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  acts_as_token_authentication_handler_for User, fallback: :exception, except: [:facebook]
+  def facebook
+    binding.pry
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      wallet = Wallet.find_or_create_by(address: Wallet.create_address, user_id: current_user.id)
+      Coin.find_or_create_by(wallet_id: wallet.id)
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to '/'
+  end
+end
--- rails-etherum-api/config/routes.rb
+++ rails-etherum-api/config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions, :registrations, :passwords]
+  devise_for :users, skip: [:sessions, :registrations, :passwords, :omniauth_callbacks]
 
   namespace :api, defaults: { format: :json } do
     get '/users', to: "users#index"
 
     devise_scope :user do
-      post   'auth/login',     to: 'sessions#create'
-      delete 'auth/logout',    to: 'sessions#delete'
-      post   'auth/users',     to: 'registrations#create'
-      delete 'auth/users/:id', to: 'registrations#delete'
-      patch  'auth/users/:id', to: 'registrations#update'
+      get    'users/auth/facebook/:id',           to:  'omniauth_callbacks#passthru'
+      post   'users/auth/facebook/:id',           to:  'omniauth_callbacks#passthru'
+      get    'users/auth/facebook/callback/:id',  to:  'omniauth_callbacks#facebook'
+      post   'users/auth/facebook/callback/:id',  to:  'omniauth_callbacks#facebook'
+      post   'auth/login',                        to:  'sessions#create'
+      delete 'auth/logout',                       to:  'sessions#delete'
+      post   'auth/users',                        to:  'registrations#create'
+      delete 'auth/users/:id',                    to:  'registrations#delete'
+      patch  'auth/users/:id',                    to:  'registrations#update'
     end
   end
 end
