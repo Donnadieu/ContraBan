b48c18be46dda649dfefbde26164d8598107194d
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 13:29:16 -0400

    Adding form using redux-form

--- rails-etherum-client/src/index.js
+++ rails-etherum-client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux'
+import {reducer as formReducer} from 'redux-form';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = {
+  form: formReducer
+}
+
+const reducer = combineReducers(reducers)
+
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ ()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+  );
 registerServiceWorker();
