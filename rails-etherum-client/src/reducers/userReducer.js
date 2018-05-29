a227521c06d3796d8094f125942004456036b35a
Donnadieu <donnadieu.86@gmail.com>
2018-05-29 19:28:38 -0400

    Created action and reducer for user login

--- rails-etherum-client/src/reducers/userReducer.js
+++ rails-etherum-client/src/reducers/userReducer.js
@@ -1,6 +1,8 @@
 export default function userReducer(state= [], action) {
   switch ( action.type ) {
-    case "USER_LOGIN"
+    case "USER_LOGIN":
       return state;
+    default:
+      return state
   }
 }
--- rails-etherum-client/src/store.js
+++ rails-etherum-client/src/store.js
@@ -3,13 +3,17 @@ import{
   applyMiddleware,
   combineReducers
 } from 'redux'
-
 import thunk from 'redux-thunk'
+import userReducer from './reducers/userReducer';
+
+const reducers = combineReducers({
+  userReducer
+})
 
 const middleware = [thunk]
 
 export default createStore(
-  combineReducers,
+  reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleware),
 )
