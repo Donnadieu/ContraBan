13b3b7670ba7169535ea9a71e94cc790d18ef6ed
Donnadieu <donnadieu.86@gmail.com>
2018-05-30 20:14:14 -0400

    Styling login form

--- rails-etherum-client/src/components/LoginForm.js
+++ rails-etherum-client/src/components/LoginForm.js
@@ -5,29 +5,36 @@ import { connect } from 'react-redux'
 
 const LoginForm = ({ handleSubmit }) => {
   return(
-    <form onSubmit={ handleSubmit }>
-      <div>
-        <label><strong>Email</strong></label>
+    <div>
+      <h1>Please Login</h1>
+      <form onSubmit={ handleSubmit }>
+        <div>
+          <label><strong>Email</strong></label>
+          <br></br>
+          <Field
+            name="email"
+            component="input"
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <label><strong>Password</strong></label>
+          <br></br>
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
         <br></br>
-        <Field
-          name="email"
-          component="input"
-          type="text"
-          placeholder="Email"
-        />
-      </div>
-      <div>
-        <label><strong>Password</strong></label>
-        <br></br>
-        <Field
-          name="password"
-          component="input"
-          type="password"
-          placeholder="Password"
-        />
-      </div>
-      <button type="submit" label="submit">Login</button>
-    </form>
+        <button type="submit" label="submit">Login</button>
+      </form>
+    </div>
+
+
+
   )
 }
 
