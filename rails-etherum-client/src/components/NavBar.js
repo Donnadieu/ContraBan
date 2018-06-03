b000fb28b62c00e806aa1e9c002029140f8bd2c0
Donnadieu <donnadieu.86@gmail.com>
2018-06-03 10:24:25 -0400

    applied conditional rendering to navbar

--- rails-etherum-client/src/components/NavBar.js
+++ rails-etherum-client/src/components/NavBar.js
@@ -4,27 +4,46 @@ import { connect } from 'react-redux'
 import { logoutUser } from '../actions/actionCreators'
 import { bindActionCreators } from 'redux';
 
-const NavBar = (props) => {
+const NavBar = ({currentUser, logoutUser}) => {
   const handleSubmit = () => {
-    return props.logoutUser(props.currentUser)
+    return logoutUser(currentUser)
+  }
+
+  const renderLinks = (currentUser) => {
+    if (currentUser.is_authenticated) {
+      return (
+        <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+          <NavLink
+            style={{ marginRight: '10px' }}
+            to="/dashboard"
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            style={{ marginRight: '10px' }}
+            to="/login"
+            onClick={handleSubmit}
+          >
+            Log Out
+          </NavLink>
+        </div>
+      )
+    } else {
+      return(
+        <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+          <NavLink
+            style={{ marginRight: '10px' }}
+            to="/dashboard"
+          >
+            Dashboard
+          </NavLink>
+        </div>
+      )
+    }
   }
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink
-        style={{ marginRight: '10px' }}
-        to="/dashboard"
-      >
-        Dashboard
-      </NavLink>
-      <NavLink
-        style={{ marginRight: '10px' }}
-        to="/login"
-        onClick={handleSubmit}
-      >
-        Log Out
-      </NavLink>
-    </div>
-  );
+    renderLinks(currentUser)
+  )
 }
 
 const mapStateToProps = function(state){
