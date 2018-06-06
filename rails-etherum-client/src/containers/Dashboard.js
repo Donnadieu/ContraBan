f778b25a1213be3dfe693ed8bfe12e3df882ea0a
Donnadieu <donnadieu.86@gmail.com>
2018-06-06 16:29:39 -0400

    Dispatch action to fect all contracts

--- rails-etherum-client/src/containers/Dashboard.js
+++ rails-etherum-client/src/containers/Dashboard.js
@@ -2,8 +2,14 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
 import ContractsPage from './ContractPage'
+import { bindActionCreators } from 'redux';
+import { fetchContracts } from '../actions/actionCreators'
 
 class Dashboard extends Component {
+  componentDidMount() {
+    const currentUser = this.props.currentUser
+    return fetchContracts(currentUser)
+  }
   render() {
     const { currentUser } = this.props
     return(
@@ -15,10 +21,16 @@ class Dashboard extends Component {
   }
 }
 
-const mapStateToProps = function(state){
+const mapStateToProps = (state) => {
   return {
     currentUser: state.currentUser,
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchContracts
+  }, dispatch)
 }
 
 export default withRouter(connect(mapStateToProps)(Dashboard))
