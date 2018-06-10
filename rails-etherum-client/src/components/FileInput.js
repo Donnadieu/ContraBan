50741ce72868ef887b490f041ff57c822f66993d
Donnadieu <donnadieu.86@gmail.com>
2018-06-10 10:36:08 -0400

    Added a component to deal with image upload

---
+++ rails-etherum-client/src/components/FileInput.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    const { input: { onChange } } = this.props
+    onChange(e.target.files[0])
+  }
+
+  render() {
+    const { input: { value } } = this.props
+
+    return (<input
+      type="file"
+      onChange={this.onChange}
+    />)
+  }
+}
+
+export default withRouter(FileInput)
