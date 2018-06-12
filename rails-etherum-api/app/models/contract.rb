0590587ab753c2333ffc76408afeecf0e8a8461d
Donnadieu <donnadieu.86@gmail.com>
2018-06-12 13:51:30 -0400

    Validate presence of image, info and name

--- rails-etherum-api/app/models/contract.rb
+++ rails-etherum-api/app/models/contract.rb
@@ -1,7 +1,10 @@
 class Contract < ApplicationRecord
-   has_many :histories
-   has_many :users, through: :histories
-   mount_uploader :avatar, ImageUploader
+  has_many :histories
+  has_many :users, through: :histories
+  validates :product_name, presence: true
+  validates :product_info, presence: true
+  validates :image, presence: true
+  mount_uploader :image, ImageUploader
 
    def current_owner
     self.histories.last.user_id
