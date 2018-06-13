b585df5d1bc10c6e54da735fa4ca3b8606d4d6df
Donnadieu <donnadieu.86@gmail.com>
2018-06-13 14:55:06 -0400

    Define method to get the last price from contract history

--- rails-etherum-api/app/serializers/contract_serializer.rb
+++ rails-etherum-api/app/serializers/contract_serializer.rb
@@ -1,4 +1,9 @@
 class ContractSerializer < ActiveModel::Serializer
-  attributes :id, :blockchain_id, :created_at, :product_name, :product_info, :histories, :image
+  attributes :id, :blockchain_id, :created_at, :product_name, :product_info, :price, :histories, :image
   has_many :histories
+
+  def price
+    object.histories.last.price
+  end
+
 end
