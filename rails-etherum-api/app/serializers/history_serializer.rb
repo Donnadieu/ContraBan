e388a3199cb841ad1bc24f029e9a43b940baa0ff
Donnadieu <donnadieu.86@gmail.com>
2018-06-13 11:56:02 -0400

    Serialize the price attribute

--- rails-etherum-api/app/serializers/history_serializer.rb
+++ rails-etherum-api/app/serializers/history_serializer.rb
@@ -1,5 +1,5 @@
 class HistorySerializer < ActiveModel::Serializer
-  attributes :user_id, :contract_id, :created_at
+  attributes :user_id, :contract_id, :price, :created_at
   belongs_to :user
   belongs_to :contract
 end
