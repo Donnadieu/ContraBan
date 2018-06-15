9740cecb56c7384a4ad22b146bde0bd067042acc
Donnadieu <donnadieu.86@gmail.com>
2018-06-15 11:07:41 -0400

    Add current contracts method and serialize result

--- rails-etherum-api/app/serializers/user_serializer.rb
+++ rails-etherum-api/app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :authentication_token, :current_contracts
+  attributes :id, :email, :authentication_token
+   has_many :current_contracts, each_serializer: ContractSerializer
+
+  def current_contracts
+    object.contracts.find_all{ |contract| contract.current_owner == object.id }
+  end
 end
