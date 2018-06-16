1aea07dcbad955e6d9de0ae3b06b8fd813522591
Donnadieu <donnadieu.86@gmail.com>
2018-06-16 12:18:48 -0400

    Add the validation for info and name

--- rails-etherum-api/app/models/contract.rb
+++ rails-etherum-api/app/models/contract.rb
@@ -4,6 +4,7 @@ class Contract < ApplicationRecord
   validates :product_name, presence: true
   validates :product_info, presence: true
   validates :image, presence: true
+  validate :name_and_info_cannot_be_undfeined
 
   mount_uploader :image, ImageUploader
 
@@ -13,5 +14,12 @@ class Contract < ApplicationRecord
 
    def price
      self.histories.last.transfer_price
+   end
+
+   def name_and_info_cannot_be_undfeined
+     if product_info == "undefined" || product_name == "undefined"
+       errors.add(:product_info, "cannot be blank or named undfeined")
+       errors.add(:product_name, "cannot be blank or named undfeined")
+     end
    end
 end
