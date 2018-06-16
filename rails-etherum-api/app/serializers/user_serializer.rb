56aafd47652132a63dff5c2397928d5c4ef1980d
Donnadieu <donnadieu.86@gmail.com>
2018-06-16 12:20:44 -0400

    Move the current contracts method to the model and not the serializer

--- rails-etherum-api/app/serializers/user_serializer.rb
+++ rails-etherum-api/app/serializers/user_serializer.rb
@@ -1,8 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :authentication_token
    has_many :current_contracts, each_serializer: ContractSerializer
-
-  def current_contracts
-    Contract.all.find_all{ |contract| contract.current_owner == object.id }
-  end
 end
