e4646ca6cba92f9dff574f5b5afb5ff25edb4a2e
Donnadieu <donnadieu.86@gmail.com>
2018-06-20 21:17:47 -0400

    Ability to download QRcode

--- rails-etherum-client/src/components/QRcode.js
+++ rails-etherum-client/src/components/QRcode.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { QRCode } from 'react-qr-svg';
+import QRCode from 'qrcode.react';
 
-const QRcode = ({contract}) => {
+const QRcode = ({contract, self}) => {
+  const handleClick = (event) => {
+    let canvas = event.currentTarget
+    var link = document.createElement('a')
+    link.download = `${contract.blockchain_id}`
+    link.href = canvas.toDataURL("image/png;base64")
+    link.click()
+  }
   return(
-    <div align="center">
-      <h2>Here is your Code</h2>
-      <QRCode value={window.location.href.split("/")[0] + "//" + window.location.href.split("/")[2] + "/contracts/" + contract.blockchain_id} style={{ width: 256 }}/>
+    <div align="center" >
+      <h2>Click on Code to Download</h2>
+      <br></br>
+      <QRCode size={200} onClick={(event) => handleClick(event)} value={window.location.href.split("/")[0] + "//" + window.location.href.split("/")[2] + "/contracts/" + contract.blockchain_id}/>
     </div>
   )
 }
