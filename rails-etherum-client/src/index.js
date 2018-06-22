c687a4e7c1f59417b0b151394be6df915b4cf2e8
Donnadieu <donnadieu.86@gmail.com>
2018-06-22 11:22:06 -0400

    Install bootstrap

--- rails-etherum-client/src/index.js
+++ rails-etherum-client/src/index.js
@@ -2,12 +2,12 @@ import {Router} from "react-router-dom"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-
-import './index.css'
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import registerServiceWorker from './registerServiceWorker'
 import history from './history'
 import App from './containers/App'
-import { persistor, store } from './store';
+import { persistor, store } from './store'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 
 ReactDOM.render(
