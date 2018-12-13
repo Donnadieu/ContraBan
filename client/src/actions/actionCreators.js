4837fe3531151c11c5528a2de79503fde801d7fe
Donnadieu <donnadieu.86@gmail.com>
2018-12-13 18:48:15 -0500

    refactoring

--- client/src/actions/actionCreators.js
+++ client/src/actions/actionCreators.js
@@ -4,7 +4,7 @@ import history from '../history'
 export const loginUser = (values) => {
   return (dispatch) => {
     dispatch({type: 'LOGGIN_ATTEMPT'})
-    return fetch(`http://localhost:3001/api/auth/login`, {
+    return fetch(`https://contraban.herokuapp.com/api/auth/login`, {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(
@@ -46,7 +46,7 @@ export const loginUser = (values) => {
 export const logoutUser = (currentUser) => {
   return (dispatch) => {
     dispatch({type: 'LOGGING_USER_OUT'})
-    return fetch(`http://localhost:3001/api/auth/logout`,{
+    return fetch(`https://contraban.herokuapp.com/api/auth/logout`,{
       method: 'delete',
       headers: {
         'Content-Type': 'application/json',
@@ -70,7 +70,7 @@ export const logoutUser = (currentUser) => {
 export const signupUser = (values) => {
   return (dispatch) => {
     dispatch({type: 'SIGNUP_ATTEMPT'})
-    return fetch(`http://localhost:3001/api/auth/users`, {
+    return fetch(`https://contraban.herokuapp.com/api/auth/users`, {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(
@@ -111,7 +111,7 @@ export const signupUser = (values) => {
 export const fetchContracts = (currentUser) => {
   return (dispatch) => {
     dispatch({type: 'LOADING_CONTRACTS'})
-    return fetch(`http://localhost:3001/api/auth/contracts`, {
+    return fetch(`https://contraban.herokuapp.com/api/auth/contracts`, {
       method: 'get',
       headers: {
         'Content-Type': 'application/json',
@@ -153,7 +153,7 @@ export const createContract = (values, currentUser) => {
     formData.append("image", values.image)
     formData.append("price", values.price)
     dispatch({type: 'CREATING_CONTRACT'})
-    return fetch(`http://localhost:3001/api/auth/contracts`, {
+    return fetch(`https://contraban.herokuapp.com/api/auth/contracts`, {
       method: 'post',
       headers: {
         'X-User-Email': currentUser.email,
@@ -190,7 +190,7 @@ export const createContract = (values, currentUser) => {
 export const transferContract = (values, currentUser, dispatch, contract) =>{
   return (dispatch) => {
     dispatch({type: 'TRANSFERING_CONTRACT'})
-    return fetch(`http://localhost:3001/api/auth/contracts/${contract.blockchain_id}`, {
+    return fetch(`https://contraban.herokuapp.com/api/auth/contracts/${contract.blockchain_id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
